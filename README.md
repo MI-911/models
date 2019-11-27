09c216039e00fd77d324f140f450efb60262fab3
Minibrams <anders@brams.dk>
2019-11-27 09:41:31 +0100

    Added a final-only filter to download script

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# models
+# Models
 Learning models and data processing libraries.
+
+## Download MindReader dataset
+Run the download script in `./data/download_data.py`. By default, the `ratings.csv` and `entities.csv` files will be saved in `./data/mindreader/`. Make sure this directory exists beforehand.
--- data/download_data.py
+++ data/download_data.py
@@ -2,14 +2,17 @@ from os.path import join
 import requests
 
 
-def download_mindreader(save_to='./data/mindreader'):
+def download_mindreader(save_to='./data/mindreader', only_completed=False):
     """
-    Downloads the mindreader dataset (ratings.csv and entities.csv) and
-    stores the files in ./<save_to>/.
-    :param save_to:
+    Downloads the mindreader dataset.
+    :param save_to: Directory to save ratings.csv and entities.csv.
+    :param only_completed: If True, only downloads ratings for users who reached the final screen.
     """
     ratings_url = 'https://mindreader.tech/api/ratings'
     entities_url = 'https://mindreader.tech/api/entities'
+
+    if only_completed:
+        ratings_url += '?final=yes'
 
     ratings_response = requests.get(ratings_url)
     entities_response = requests.get(entities_url)
@@ -21,4 +24,4 @@ def download_mindreader(save_to='./data/mindreader'):
 
 
 if __name__ == '__main__':
-    download_mindreader(save_to='../data/mindreader')
+    download_mindreader(save_to='../data/mindreader', only_completed=True)
